63a13052d1ae1deca5f6ad3141e9729a589c883b
eadaoinb <eadaoinburgess@MacBook-Pro.local>
2017-11-10 16:43:15 +0000

    make my 2nd change

--- index.js
+++ index.js
@@ -22,16 +22,12 @@ app.get('/projects', function(request, response) {
   response.render('pages/projects');
 });
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
-
-app.get('/contact', function(request, response) {
-  response.render('pages/contact');
 });
 
 
